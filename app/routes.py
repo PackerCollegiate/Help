07c4ae062adde70b8223adb2eb265b97062257d8
cableich <122039795+cableich@users.noreply.github.com>
2023-06-08 14:47:12 -0400

    formatting update

--- app/routes.py
+++ app/routes.py
@@ -49,7 +49,7 @@ def explore():
         if posts.has_next else None
     prev_url = url_for('explore', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('index.html', title='Explore', posts=posts.items,
+    return render_template('explore.html', title='Explore', posts=posts.items,
                            next_url=next_url, prev_url=prev_url)
 
 
